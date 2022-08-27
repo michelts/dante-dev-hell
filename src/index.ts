2087cdf0410abf846dc9bbc314362aed1792c01a
Michel Sabchuk <michel@turbosys.com.br>
2022-08-27 14:34:26 -0300

    Restructure the app code

--- src/index.ts
+++ src/index.ts
@@ -4,66 +4,86 @@ import generateMonster, { BaseMonster } from "./monsters";
 import Character from "./character";
 import Lifes from "./lifes";
 
-window.gameCanvas = initCanvas();
-
 function initCanvas() {
   const { canvas } = init();
   initKeys();
   return canvas;
 }
 
-function app() {
-  const monsterGenerator = generateMonster();
-  const splash = new Splash();
+class App {
+  play: boolean | null = null;
+  splash: Splash;
+  monster: BaseMonster;
+  monsterGenerator: generateMonster;
+  character: Character;
+  lifes: Lifes;
 
-  let play = false;
-  let monster: BaseMonster;
-  let character: Character;
-  let lifes: Lifes;
+  init() {
+    this.splash = new Splash();
+    const loop = GameLoop({
+      update: this.update.bind(this),
+      render: this.render.bind(this),
+    });
+    loop.start();
+    this.attachEventListeners();
+  }
 
-  const loop = GameLoop({
-    update: () => {
-      if (play) {
-        monster.fall();
-        character.killOnCollide(monster);
-        character.update();
-        lifes.update();
-      }
-    },
-    render: function () {
-      if (play) {
-        monster.render();
-        character.render();
-        lifes.render();
-      } else {
-        splash.render();
-      }
-    },
-  });
-
-  loop.start();
-
-  on("killed", () => {
-    lifes.discountLife();
-    if (!lifes.isLive()) {
-      play = false;
+  render() {
+    if (this.play === true) {
+      this.monster.render();
+      this.character.render();
+      this.lifes.render();
+    } else {
+      this.splash.render();
     }
-  });
+  }
 
-  onKey("space", () => {
-    play = true;
-    monster = monsterGenerator.next().value;
-    character = new Character();
-    lifes = new Lifes();
-  });
+  update() {
+    if (this.play === true) {
+      this.monster.fall();
+      this.character.killOnCollide(this.monster);
+      this.character.update();
+      this.lifes.update();
+    }
+  }
 
-  on("monsterDead", () => {
-    monster = monsterGenerator.next().value;
-  });
+  attachEventListeners() {
+    onKey("space", this.playPauseGame.bind(this));
+    onKey("esc", this.stopGame.bind(this));
+    on("killed", this.handleHeroKilled.bind(this));
+    on("monsterDead", this.respawnMonster.bind(this));
+  }
 
-  onKey("esc", () => {
-    play = false;
-  });
+  playPauseGame() {
+    if (this.play === null) {
+      this.initializeObjects();
+      this.play = true;
+    } else {
+      this.play = !this.play;
+    }
+  }
+
+  stopGame() {
+    this.play = null;
+  }
+
+  initializeObjects() {
+    this.monsterGenerator = generateMonster();
+    this.monster = this.monsterGenerator.next().value;
+    this.character = new Character();
+    this.lifes = new Lifes();
+  }
+
+  handleHeroKilled() {
+    this.lifes.discountLife();
+    if (!this.lifes.isLive()) {
+      this.play = false;
+    }
+  }
+
+  respawnMonster() {
+    this.monster = this.monsterGenerator.next().value;
+  }
 }
 
 declare global {
@@ -72,4 +92,6 @@ declare global {
   }
 }
 
-app();
+window.gameCanvas = initCanvas();
+const app = new App();
+app.init();
