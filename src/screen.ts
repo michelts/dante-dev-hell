2c8e7b369723e4101530d3eed93c52d165c1acfc
Michel Sabchuk <michel@turbosys.com.br>
2022-09-02 07:57:38 -0300

    Hold content on fixed layer without pointer events and control its visibility

--- src/screen.ts
+++ src/screen.ts
@@ -1,19 +1,63 @@
 import { GameStatus } from "./types";
-import Layers from "./layers";
-import Content from './content';
+
+const infoRenderers = {
+  [GameStatus.Stop]: renderNotStarted,
+  [GameStatus.Complete]: renderComplete,
+  [GameStatus.GameOver]: renderGameOver,
+};
 
 export default class Screen {
   constructor() {
-    this.layers = new Layers();
-    this.content = new Content();
+    this.infoLayer = document.getElementById("info");
   }
 
   render(newGameStatus: GameStatus): void {
     if (this.currentGameStatus === newGameStatus) {
       return;
     }
-    this.layers.toggle(newGameStatus);
-    this.content.toggle(newGameStatus);
     this.currentGameStatus = newGameStatus;
+    this.toggleInfoVisibility();
+    this.renderInfo();
   }
+
+  private toggleInfoVisibility() {
+    const isStandBy =
+      this.currentGameStatus !== GameStatus.Play &&
+      this.currentGameStatus !== GameStatus.Pause;
+    this.infoLayer.style.visibility = isStandBy ? "visible" : "hidden";
+  }
+
+  private renderInfo() {
+    const renderFunc = infoRenderers[this.currentGameStatus];
+    if (renderFunc !== undefined) {
+      this.infoLayer.innerHTML = renderFunc();
+    }
+  }
+}
+
+function renderNotStarted() {
+  return `
+    <h1>Dante's Dev Hell</h1>
+    <p>You are Dante, a developer who died and went to <b>the Developer's Hell</b>.</p>
+    <p>Yeah, how could you imagine those <u>silly jokes</u> about javascript would bring you there?</p>
+    <p>With the help of your mentor Virgilio, <i>seek your redemption</i> sailing the Acheron river through the deeps of the programming hell!</p>
+    <p><u>Tap or press any key to start</u></p>
+  `;
+}
+
+function renderGameOver() {
+  return `
+    <h1>Dante's Dev Hell</h1>
+    <p>No good! Your soul were consumed by the hatred against <i>PHP typing system</i>.</p>
+    <p><u>Tap or press any key to try again</u></p>
+  `;
+}
+
+function renderComplete() {
+  return `
+    <h1>Dante's Dev Hell</h1>
+    <p>Congratulations! You crossed the <b>deeps of the Programming Hell</b> with resilience and perseverance.
+    <p>You got <i>your redemption</i> and may now live your life with kindness and respect to all the languages, even PHP.</p>
+    <p><u>Tap or press any key to play again</u></p>
+  `;
 }
