6456572221f31c77f4d74e1e91cd695bc2f2165d
Michel Sabchuk <michel@turbosys.com.br>
2022-09-02 18:48:47 -0300

    Force fullscreen if in a portrait screen

--- src/index.ts
+++ src/index.ts
@@ -17,11 +17,20 @@ function initCanvas() {
 
 function createCanvas() {
   const container = document.getElementById("game");
+  if (isPortrait()) {
+    container.parentNode.classList.add("full");
+  }
   const canvas = document.createElement("canvas");
   canvas.setAttribute("width", container.offsetWidth);
   canvas.setAttribute("height", container.offsetHeight);
   container.appendChild(canvas);
   return canvas;
+}
+
+function isPortrait() {
+  return (
+    document.documentElement.clientHeight > document.documentElement.clientWidth
+  );
 }
 
 window.gameCanvas = initCanvas();
