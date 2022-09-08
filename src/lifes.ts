f6ab38acc25b0d710b3ef81190f911412efded7e
Michel Sabchuk <michel@turbosys.com.br>
2022-09-07 22:49:40 -0300

    Add heart image

--- src/lifes.ts
+++ src/lifes.ts
@@ -17,9 +17,9 @@ export default class Lifes {
   update(): void {
     this.sprites.forEach((sprite, index) => {
       if (index < this.lifesCount) {
-        sprite.color = "red";
+        sprite.fillColor = "rgba(255, 0, 0, 1)";
       } else {
-        sprite.color = "black";
+        sprite.fillColor = "rgba(255, 0, 0, 0.3)";
       }
     });
   }
@@ -37,8 +37,8 @@ export default class Lifes {
   }
 
   private createSprites() {
-    const width = 10;
-    const height = 10;
+    const width = 16;
+    const height = 16;
     const margin = 5;
     this.sprites = Array(3)
       .fill(null)
@@ -46,9 +46,14 @@ export default class Lifes {
         Sprite({
           x: margin * (index + 1) + width * index,
           y: window.gameCanvas.height - height - margin,
-          width: 10,
-          height: 10,
-          color: "red",
+          width,
+          height,
+          render: function () {
+            const path = new Path2D(heartPath);
+            this.context.fillStyle = this.fillColor;
+            this.context.filter = "drop-shadow(2px 2px 3px rgba(0, 0, 0, 1))";
+            this.context.fill(path);
+          },
         })
       );
   }
@@ -60,3 +65,9 @@ export default class Lifes {
     }
   }
 }
+
+const heartPath = `
+  M 2.57,0.10 C -0.12,0.65 -0.16,3.80 0.118,5.86 0.66,10.10 4.04,13.51
+  8.00,16 11.97,13.501 15.34,10.10 15.89,5.86 16.16,3.79 16.12,0.65
+  13.43,0.10 11.06,-0.38 9.37,0.96 8.00,3.32 6.38,0.89 5.00,-0.38 2.57,0.10 Z
+`;
